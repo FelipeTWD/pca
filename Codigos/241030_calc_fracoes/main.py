6f606b02b51a1aaee334263fbba376375418612d
Gabriel Machado <gabriel.rmachado10@gmail.com>
2024-10-30 22:03:56 -0300

    fix: refatoração visando o uso da sobrecarga de operadores em Python.

--- Codigos/241030_calc_fracoes/main.py
+++ Codigos/241030_calc_fracoes/main.py
@@ -1,17 +1,24 @@
+from sys import stderr
 from fracao import Fracao
-from calculadora_fracao import Calculadora_Fracao
+
 
 def main() -> None:
     
     # 'fracao1' e 'fracao2' são OBJETOS da CLASSE Fracao.
-    fracao1: Fracao = Fracao(2, 3)
-    fracao2: Fracao = Fracao(4, 7)
+    try:
+        fracao1: Fracao = Fracao(2, 3)
+        fracao2: Fracao = Fracao(27, 0)
+        
+        resultado = fracao1 - fracao2
+        
+        print(f'O resultado da multiplicação de {fracao1} por {fracao2} é: {resultado} e em decimal é: {resultado.transformar_decimal():.5f}')
     
-    # 'calculadora' é um OBJETO da CLASSE Calculadora_Fracao.
-    calculadora: Calculadora_Fracao = Calculadora_Fracao(fracao1, fracao2)
-    resultado: Fracao = calculadora.somar()
-    
-    print(f'O resultado da multiplicação de {fracao1} por {fracao2} é: {resultado} e em decimal é: {resultado.transformar_decimal():.5f}')
+    except ValueError as e:
+        print(f'Erro de divisão por zero: {e}', file=stderr)
+        
+    except Exception as e:
+        print(f'Um erro inesperado ocorreu: {e}', file=stderr)
+
 
 if __name__ == '__main__':
     main()
